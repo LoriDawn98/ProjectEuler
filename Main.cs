8a9de4ddbf43efe3aaaf5cc6452ef231c4ec3555
Lori de Boer <loridawn98@gmail.com>
2022-12-22 08:38:56 -0600

    Initial commit with Euler problem 1

---
+++ Main.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Project_Euler;
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello, World!");
+
+        //Euler Problem 1
+        Problem1 problem1 = new Problem1();
+        List<int> multiplesOf3and5 = problem1.getMultiplesOfTwoNumbers(3, 5, 1000);
+        int sum = 0;
+        sum = multiplesOf3and5.Sum();
+        Console.WriteLine(sum);
+        Console.WriteLine(String.Join(", ", multiplesOf3and5));
+       // multiplesOf3and5.ForEach(Console.WriteLine);
+
+
+    }
+}
+
+
+
+
---
+++ Problem1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Euler
+{
+    internal class Problem1
+    {
+        public List<int> getMultiplesOfTwoNumbers(int value1, int value2, int range)
+        {
+            List<int> valuesMultiples = new List<int>();
+            for(int i=1; i<range; i++)
+            {
+                if(i%value1 == 0 || i%value2 == 0)
+                {
+                    valuesMultiples.Add(i);
+                }
+            }
+            return valuesMultiples;
+        }
+    }
+}

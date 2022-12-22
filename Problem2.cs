f61f51edd943f6a1a5dfdb74620e3adff6f6ae7c
Lori de Boer <loridawn98@gmail.com>
2022-12-22 10:01:02 -0600

    Problem 2 Solution

---
+++ Problem2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Euler
+{
+    internal class Problem2
+    {
+        public int getSumOfEvenFibonacci(int range)
+        {
+            int a = 1, b = 2, c = 0, sum = 0;
+
+            while (b < range)
+            {
+                if(b%2 == 0)
+                {
+                    sum += b;
+                }
+
+                c = a + b;
+                a = b;
+                b = c;
+            }
+
+            return sum;
+        }
+    }
+}
